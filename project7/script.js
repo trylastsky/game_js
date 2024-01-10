052babde1bdb7891dae8145402a0fd61f075796f
desnat <deleron972@gmail.com>
2024-01-10 17:40:17 +0400

    project7 full

--- project7/script.js
+++ project7/script.js
@@ -1,13 +1,18 @@
 window.addEventListener('load', function() {
         const canvas = document.getElementById('canvas1');
         const ctx = canvas.getContext('2d');
-        canvas.width = 800;
+        canvas.width = 1400;
         canvas.height = 720;
         let enemies = [];
+        let score = 0;
+        let gameOver = false;
+        const fullScreenButton = document.getElementById('fullScreenButton');
 
         class InputHandler {
             constructor() {
                 this.keys = [];
+                this.touchY = '';
+                this.touchTreshold = 30;
                 window.addEventListener('keydown', e => {
                     if ((e.key === 'ArrowDown' ||
                         e.key === 'ArrowUp' ||
@@ -16,6 +21,7 @@ window.addEventListener('load', function() {
                         && this.keys.indexOf(e.key) === -1) {
                         this.keys.push(e.key);
                     }
+                    if (e.key === 'Enter' && gameOver) restartGame();
                 });
 
                 window.addEventListener('keyup', e => {
@@ -26,7 +32,24 @@ window.addEventListener('load', function() {
                         this.keys.splice(this.keys.indexOf(e.key), 1);
                     }
                 });
-            }
+                window.addEventListener('touchstart', e => {
+                    this.touchY = e.changedTouches[0].pageY;
+                });
+                window.addEventListener('touchmove', e => {
+                    const swipeDistance = e.changedTouches[0].pageY - this.touchY;
+                    if (swipeDistance < -this.touchTreshold && this.keys.indexOf('ArrowUp') === -1) {
+                        this.keys.push('ArrowUp');
+                    }
+                    if (swipeDistance > this.touchTreshold && this.keys.indexOf('ArrowDown') === - 1) {
+                        this.keys.push('ArrowDown');
+                        if (gameOver) restartGame();
+                    }
+                });
+                window.addEventListener('touchend', e => {
+                    this.keys.splice(this.keys.indexOf('swipe up'), 1);
+                    this.keys.splice(this.keys.indexOf('swipe down'), 1);
+                });
+                }
         }
 
         class Player {
@@ -35,21 +58,47 @@ window.addEventListener('load', function() {
                 this.gameHeight = gameHeight;
                 this.width = 200;
                 this.height = 200;
-                this.x = 0;
+                this.x = 100;
                 this.y = this.gameHeight - this.height;
                 this.image = document.getElementById('playerImage');
                 this.frameX = 0;
                 this.frameY = 0;
+                this.maxFrame = 8;
+                this.fps = 20;
+                this.frameTimer = 0;
+                this.frameInterval = 1000/this.fps;
                 this.speed = 0;
                 this.vy = 0;
                 this.weight = 1;
             }
+            restart() {
+                this.x = 100;
+                this.y = this.gameHeight - this.height;
+                this.maxFrame = 8;
+                this.frameY = 0;
+            }
             draw(context) {
-                context.fillStyle = "white";
                 context.drawImage(this.image, this.frameX * this.width, this.frameY
                     * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
             }
-            update(input) {
+            update(input, deltaTime, enemies) {
+                //collision detection
+                enemies.forEach(enemy => {
+                    const dx = (enemy.x + enemy.width/2 - 20)- (this.x + this.width/2);
+                    const dy = (enemy.y + enemy.height/2) - (this.y + this.height/2 + 20);
+                    const distance = Math.sqrt(dx * dx + dy * dy);
+                    if (distance < enemy.width/3 + this.width/3){
+                        gameOver = true;
+                    }
+                });
+                //sprite animations
+                if (this.frameTimer > this.frameInterval) {
+                    if (this.frameX >= this.maxFrame) this.frameX = 0;
+                    else this.frameX++;
+                    this.frameTimer = 0;
+                } else {
+                    this.frameTimer += deltaTime;
+                }
                 if (input.keys.indexOf('ArrowRight') > -1) {
                     this.speed = 5;
                 } 
@@ -70,9 +119,11 @@ window.addEventListener('load', function() {
                 this.y += this.vy;
                 if (!this.onGround()) {
                     this.vy += this.weight;
+                    this.maxFrame = 5;
                     this.frameY = 1;
                 } else {
                     this.vy = 0;
+                    this.maxFrame = 8;
                     this.frameY = 0;
                 }
                 if (this.y > this.gameHeight - this.height) this.y = this.gameHeight - this.height;
@@ -92,6 +143,9 @@ window.addEventListener('load', function() {
                 this.width = 2400;
                 this.height = 720;
                 this.speed = 7;
+            }
+            restart() {
+                this.x = 0;
             }
             draw(context) {
                 context.drawImage(this.image,this.x,this.y,this.width,this.height);
@@ -113,13 +167,31 @@ window.addEventListener('load', function() {
                 this.x = this.gameWidth;
                 this.y = this.gameHeight - this.height;
                 this.frameX = 0;
+                this.maxFrame = 5;
+                this.fps = 20;
+                this.frameTimer = 0;
+                this.frameInterval = 1000/this.fps;
                 this.speed = 8;
+                this.markedForDeletion = false;
+                
             }
             draw(context) {
-                context.drawImage(this.image,0 * this.width,0,this.width,this.height, this.x, this.y, this.width, this.height);
+                context.drawImage(this.image, this.frameX * this.width,0,this.width,
+                this.height, this.x, this.y, this.width, this.height);
             }
-            update() {
+            update(deltaTime) {
+                if (this.frameTimer > this.frameInterval) {
+                    if (this.frameX >= this.maxFrame) this.frameX = 0;
+                    else this.frameX++;
+                    this.frameTimer = 0;
+                } else {
+                    this.frameTimer += deltaTime;
+                }
                 this.x -= this.speed;
+                if (this.x < 0 - this.width){
+                    this.markedForDeletion = true;
+                    score++;
+                } 
             }
         }
 
@@ -135,12 +207,47 @@ window.addEventListener('load', function() {
                 }
                 enemies.forEach(enemy => {
                 enemy.draw(ctx);
-                enemy.update();
+                enemy.update(deltaTime);
             });
+            enemies = enemies.filter(enemy => !enemy.markedForDeletion);
         }
 
-        function displayStatusText() {
+        function displayStatusText(context) {
+            context.textAlign = 'left';
+            context.fillStyle = 'black';
+            context.font = '40px Helvetica';
+            context.fillText('Score: ' + score, 20 ,50);
+            context.fillStyle = 'white';
+            context.font = '40px Helvetica';
+            context.fillText('Score: ' + score, 22 ,52);
+            if (gameOver) {
+                context.textAlign = 'center';
+                context.fillStyle = 'black';
+                context.fillText('GAME OVER, press ENTER or swipe down to restart!', canvas.width/2, 200);
+                context.fillStyle = 'white';
+                context.fillText('GAME OVER, press ENTER or swipe down to restart!', canvas.width/2 + 2, 202)
+            }
         }
+
+        function restartGame() {
+            player.restart();
+            background.restart();
+            enemies = [];
+            score = 0;
+            gameOver = false;
+            animate(0);
+        }
+
+        function toggleFullScreen() {
+            if (!document.fullscreenElement) {
+                canvas.requestFullscreen().catch(err => {
+                    alert(`Error, can't enable full-screen mode: ${err.message}`);
+                });
+            } else {
+                document.exitFullscreen();
+            }
+        }
+        fullScreenButton.addEventListener('click', toggleFullScreen);
 
         const input = new InputHandler();
         const player = new Player(canvas.width, canvas.height);
@@ -158,10 +265,10 @@ window.addEventListener('load', function() {
             background.draw(ctx);
             background.update();
             player.draw(ctx);
-            player.update(input);
+            player.update(input, deltaTime, enemies);
             handleEnemies(deltaTime);
-            requestAnimationFrame(animate);
+            displayStatusText(ctx);
+            if (!gameOver) requestAnimationFrame(animate);
         }
         animate(0);
     });
-    // video time 5 18
